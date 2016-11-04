c4b2968ef10ce9a898770a5e75aeaf3334d4b241
trepidacious <trepidacious@gmail.com>
2016-11-04 18:16:14 +0000

    Create 4 switches so we don't need registry

--- src/switch.py
+++ src/switch.py
@@ -1,11 +1,15 @@
 import energenie
 import sys
 
-if __name__ == "__main__":
+socket1     = energenie.Devices.ENER002((0xBEEF1, 1))
+socket2     = energenie.Devices.ENER002((0xBEEF1, 2))
+socket3     = energenie.Devices.ENER002((0xBEEF1, 3))
+socket4     = energenie.Devices.ENER002((0xBEEF1, 4))
 
+if __name__ == "__main__":
     argCount = len(sys.argv)
     if argCount < 3:
-        print("usage: 'python switch.py DEVICE STATE' where DEVICE is a switch in registry and STATE is on or off")
+        print("usage: 'python switch.py DEVICE STATE' where DEVICE is a number from 1 to 4 and STATE is on or off")
         sys.exit(1)
 
     name = sys.argv[1]
@@ -13,10 +17,15 @@ if __name__ == "__main__":
 
     energenie.init()
     try:
-        device = energenie.registry.get(name)
-        if device.has_switch():
-            print("Switch id %s" % device)
-            device.set_switch(on)
+#        device = energenie.registry.get(name)
+#        if device.has_switch():
+#            print("Switch id %s" % device)
+#            device.set_switch(on)
+        if name == "1" : socket1.set_switch(on)
+        if name == "2" : socket2.set_switch(on)
+        if name == "3" : socket3.set_switch(on)
+        if name == "4" : socket4.set_switch(on)
+
     finally:
         energenie.finished()
         
